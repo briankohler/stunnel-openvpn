14d1a33e7f873063f02ff991794c18741a97b358
Brian Kohler <brian.kohler@heal.com>
2021-03-22 21:49:50 -0700

    dont overwrite locally install t

--- create.sh
+++ create.sh
@@ -1,15 +1,10 @@
 #!/bin/bash 
 
 echo "Checking local system for prerequisites..."
-terraform -v > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-  echo "Downloading Terraform..."
-  wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_darwin_amd64.zip
-  unzip terraform_0.11.1_darwin_amd64.zip
-  sudo mv terraform /usr/local/bin/
-  rm terraform_0.11.1_darwin_amd64.zip
-fi
+echo "Downloading Terraform..."
+wget https://releases.hashicorp.com/terraform/0.11.1/terraform_0.11.1_darwin_amd64.zip
+unzip terraform_0.11.1_darwin_amd64.zip
+rm terraform_0.11.1_darwin_amd64.zip
 
 brew install gsed jq > /dev/null 2>&1
 echo "Prerequisites installed"
@@ -23,13 +18,13 @@ PUBLIC_IP=$(curl -s ifconfig.me)
 echo "Public IP is $PUBLIC_IP"
 echo "Creating SSH key..."
 rm -f id_rsa*
-ssh-keygen -f id_rsa -t rsa -N "" > /dev/null 2>&1
+ssh-keygen -f id_rsa -m pem -t rsa -N "" > /dev/null 2>&1
 chmod 400 id_rsa
 
 echo "Provisioning proxy..."
 
-terraform init
-terraform apply -auto-approve -var do_token=$DO_TOKEN -var public_ip="$PUBLIC_IP" -var gateway="$(netstat -anr | grep default | head -n1 | awk '{print $2}')"
+./terraform init
+./terraform apply -auto-approve -var do_token=$DO_TOKEN -var public_ip="$PUBLIC_IP" -var gateway="$(netstat -anr | grep default | head -n1 | awk '{print $2}')"
 echo "Proxy instance provisioned"
 echo "Your config is saved to client.ovpn"
 cat /tmp/proxy.ovpn > client.ovpn
--- destroy.sh
+++ destroy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash  
 
 PUBLIC_IP=$(curl -s ifconfig.me)
-echo "Stopping Tunnelblick..."
-pkill Tunnelblick
+echo "Stopping OpenVPN..."
+pkill OpenVPNConnect
 if [ -z "${DO_TOKEN}" ]
 then
   echo "Input DigitalOcean API token: "
   read DO_TOKEN
 fi
 echo "Destroying remote servers..."
-terraform destroy -force -var do_token=$DO_TOKEN -var public_ip="$PUBLIC_IP" -var gateway="$(netstat -anr | grep default | head -n1 | awk '{print $2}')"
+./terraform destroy -force -var do_token=$DO_TOKEN -var public_ip="$PUBLIC_IP" -var gateway="$(netstat -anr | grep default | head -n1 | awk '{print $2}')"
 echo "Cleaning up..."
-rm -rf stunnel.pid stunnel.pem puppet/stunnel.pem puppet/site.pp id_rsa* terraform.tfstate* .terraform *.ovpn
+rm -rf stunnel.pid terraform stunnel.pem puppet/stunnel.pem puppet/site.pp id_rsa* terraform.tfstate* .terraform *.ovpn
